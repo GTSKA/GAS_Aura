59cd961c205eb89eaf52ff69df0646ff7324804d
GTSKA <gtskatalites@gmail.com>
2024-08-04 13:45:43 -0600

    Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// No copyright on test
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// No copyright on test
+
+#pragma once
+
+#include "CoreMinimal.h"
+
