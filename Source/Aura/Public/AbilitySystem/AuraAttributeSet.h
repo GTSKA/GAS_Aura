f9d4d304e3a12781f410a75b546963c8839f6638
GTSKA <gtskatalites@gmail.com>
2024-08-24 20:05:48 -0600

    Health and Mana

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,4 +13,34 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Health, BlueprintReadOnly, Category="Aura Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, BlueprintReadOnly, Category="Aura Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Mana, BlueprintReadOnly, Category="Aura Vital Attributes");
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, BlueprintReadOnly, Category="Aura Vital Attributes");
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+
+
 };
